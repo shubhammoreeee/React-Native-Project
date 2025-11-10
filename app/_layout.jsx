0d23b6d1d9fc433e5f61489d6e0c872951e2d7d2
shubhammoreeee <shubhammore14112006@gmail.com>
2025-11-10 15:55:02 +0530

    cwe

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,13 +1,19 @@
 import { Stack } from "expo-router";
 import { useFonts } from 'expo-font';
 import { StackScreen } from "react-native-screens";
-import { ClerkProvider, SignedIn, SignedOut,tokenCache , useOAuth } from "@clerk/clerk-expo";
+import { ClerkProvider, SignedIn, SignedOut,useOAuth } from "@clerk/clerk-expo";
 import { useAuth } from "@clerk/clerk-expo";
 import * as Linking from "expo-linking";
 import * as SecureStore from "expo-secure-store";
 // import { tokenCache } from "@clerk/clerk-expo/dist/cache";
 
 const clerkKey = process.env.EXPO_PUBLIC_CLERK_PUBLISHABLE_KEY || "pk_test_bWFqb3ItYXNwLTQ5LmNsZXJrLmFjY291bnRzLmRldiQ";
+
+const tokenCache = {
+  getToken: (key) => SecureStore.getItemAsync(key),
+  saveToken: (key, value) => SecureStore.setItemAsync(key, value),
+};
+
 
 export default function RootLayout() {
 
@@ -21,10 +27,7 @@ export default function RootLayout() {
     
         <ClerkProvider 
         publishableKey={clerkKey}
-        tokenCache={tokenCache({
-        getToken: SecureStore.getItemAsync,
-        saveToken: SecureStore.setItemAsync,
-      })}
+        tokenCache={tokenCache}
     >
 
     <Stack>
